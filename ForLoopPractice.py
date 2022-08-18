607ad11328acad30f855471317dcb56e0a26596e
Matthew Yacovone <mryacovo@ncsu.edu>
2022-08-18 10:41:40 -0400

    For Loop Practice

---
+++ ForLoopPractice.py
@@ -0,0 +1,15 @@
+# For Loop Practice
+# Matthew Yacovone
+
+mySum = 0
+for i in range(7,10):
+	mySum += i
+	print(mySum)
+print("StartStop =", mySum)	# answer = 0 + 7 + 8 + 9 = 24
+
+x = 0
+for a in range(5,11,2):
+    x += a
+    print(a)
+print("StartStopStep =",x) 	# answer = 5 + 7 + 9 = 21
+
