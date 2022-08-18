a57dd35c2d8cff1b32b6a475f537633fb783028d
Matthew Yacovone <mryacovo@ncsu.edu>
2022-08-18 11:01:35 -0400

    Added last comment

--- ForLoopPractice.py
+++ ForLoopPractice.py
@@ -1,22 +1,22 @@
 # For Loop Practice
 # Matthew Yacovone
 
-mySum = 0
-for i in range(7,10):
-	mySum += i
-	print(mySum)
-print("StartStop =", mySum)	# answer = 0 + 7 + 8 + 9 = 24
-
 x = 0
-for a in range(5,11,2):
-    x += a
-    print(a)
-print("StartStopStep =",x) 	# answer = 5 + 7 + 9 = 21
+for a in range(7,10):
+	x += a
+	print(x)
+print("StartStop =", x)    # answer = 0 + 7 + 8 + 9 = 24
 
 y = 0
 for b in range(5,11,2):
-	y += b 
-	if y == 5:
+    y += b
+    print(y)
+print("StartStopStep =",y)    # answer = 0 + 5 + 7 + 9 = 21
+
+z = 0
+for c in range(5,11,2):
+	y += c 
+	if z == 5:
 		break
-		y += 1
-print("StartStepStopBreak =",y)
+		z += 1
+print("StartStepStopBreak =",z)    # answer = 5
