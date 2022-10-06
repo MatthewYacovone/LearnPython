de3426cca3751c573857532ffd144cd3fa19da22
Matthew Yacovone <mryacovo@gmail.com>
2022-10-05 22:48:13 -0400

    Added grid, changed units, and added styles

--- IntroToPlots.py
+++ IntroToPlots.py
@@ -11,15 +11,17 @@ x = [9.019,7.96,5.59,3.67,2.51,1.88,0.842]
 y = [9.25,8.33,5.9,3.97,2.83,1.99,0.9]
   
 # plotting the points 
-plt.plot(x, y)
+plt.plot(x, y,'-ko')
   
 # naming the x axis
-plt.xlabel('Voltage (V)')
+plt.xlabel('Voltage (kV)')
 # naming the y axis
 plt.ylabel('Resistance (Ohms)')
   
 # giving a title to my graph
 plt.title('Voltage vs. Measured Resistance (for the Potentiometer Experiment)')
-  
+
+# giving a grid to my graph
+plt.grid()
 # function to show the plot
 plt.show()
