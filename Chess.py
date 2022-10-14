dbb45f5cb6474cb89790e1e9225b808a220b7c66
Matthew Yacovone <mryacovo@gmail.com>
2022-10-13 23:55:31 -0400

    Skeleton of new chess project

---
+++ Chess.py
@@ -0,0 +1,30 @@
+# Chess 
+# Matthew Yacovone
+
+import board
+
+class Chess():
+    """
+    The Chess Class will represents the state of the game such as whose 
+    turn it is, a Board object, and whether the next player can en passant.
+    It also includes moving a piece and promoting a Pawn.
+    """
+    def __init__():
+        # The state of the game
+        
+    def promotion():
+        # Functionality of promoting a Pawn that has reached the 
+        # end of the board
+        
+    def move():
+        # Functionality of moving a a piece from its current location to a destination
+
+class Board():
+    """
+    The Board Class represents the configuration of the board which means 
+    where the pieces are on the board with the method being to print the board.
+    """
+    
+    
+    
+        
